4cd85b7f0d90fa462b4c2c9fdadcee0a7c28e2d3
JoseMatheus29 <mateus.alvino.101@gmail.com>
2022-08-04 17:03:00 -0300

    Adicionando teste

--- contas/tests.py
+++ contas/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from selenium import webdriver
+import unittest
 
-# Create your tests here.
+class TestGerenciador(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+    def tearDown(self):
+        self.browser.quit(self)
+
+    def test_title_in_app(self):
+        self.browser.get('http://127.0.0.1:8000/home')
+        self.assertIn('Transacao', self.browser.title)
+    
+if __name__ == 'main':
+    unittest.main(warnings=ignore)
